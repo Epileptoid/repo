772d0bb341bfeedc0c9f92d72c82ec79298f4821
Ilkhom Nazarov <nazarov.ilkhom@gmail.com>
2018-05-19 23:59:12 +0300

    UPD index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-print ("what a fuck are you doing here, dude?")
+<?php
+
+$print ("what a fuck are you doing here, dude?")
+
+$lines = file('http://inazarov.ru/');
+
+// Осуществим проход массива и выведем содержимое в виде HTML-кода вместе с номерами строк.
+foreach ($lines as $line_num => $line) {
+    echo "Строка #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br />\n";
+}
+
+?>
